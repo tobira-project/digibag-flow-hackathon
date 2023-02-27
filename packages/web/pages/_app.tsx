03890560381b46c3989b8595b685e849a5eaecce
toru-toruto <toruto.torutoru.toruto@gmail.com>
2023-02-28 04:08:15 +0900

    実装：装飾画面でインベントリ以外スクロール禁止

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -4,17 +4,19 @@ import type { AppProps } from "next/app";
 import globalData from "@/data/globalData.json";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { innerWidth, innerHeight } = useWindowSize();
+  const { innerWidth, innerHeight, displayWidth, displayHeight } = useWindowSize();
   const { mediaBorder, pcWidth, pcHeight } = globalData;
 
   return (
     <>
-      <div className="absolute inset-0 z-[100] bg-black/40">
+      <div className="absolute inset-0 z-[-100] bg-black/40">
         <div
           className="app-sp-display"
           style={{
             left: innerWidth > mediaBorder ? (innerWidth - pcWidth) / 2.0 : 0,
             top: innerWidth > mediaBorder ? (innerHeight - pcHeight) / 2.0 : 0,
+            width: displayWidth,
+            height: displayHeight
           }}
         >
           <Component {...pageProps} />
